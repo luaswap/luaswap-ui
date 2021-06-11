8062e75e5ffb62e04aa9f4c3db82276dbb505268
Theo <trungdao912@gmail.com>
2021-06-11 15:58:23 +0700

    chore: fix app does not show correct balance of user

--- src/hooks/useEthBalance.ts
+++ src/hooks/useEthBalance.ts
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import BigNumber from 'bignumber.js'
 import useWeb3 from 'hooks/useWeb3'
+import { useBlock } from 'state/hooks'
 import { getFullDisplayBalance } from 'utils/formatBalance'
 
 const useEthBalance = () => {
   const { account, chainId } = useWeb3React()
   const web3 = useWeb3()
+  const { currentBlock } = useBlock()
   const [balance, setBalance] = useState('0')
 
   useEffect(() => {
@@ -16,8 +18,7 @@ const useEthBalance = () => {
         setBalance(newBalance)
       })
     }
-  }, [web3, account, chainId])
-
+  }, [web3, account, chainId, currentBlock])
   return balance
 }
 
