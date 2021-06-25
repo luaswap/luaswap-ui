cfe1962bee928110f1a8c229017559656dccbedb
Trung Dao <trungdao912@gmail.com>
2021-06-25 10:25:32 +0700

    chore: update description for hook

--- src/hooks/useDeepMemo.ts
+++ src/hooks/useDeepMemo.ts
@@ -1,6 +1,10 @@
 import { useRef } from 'react'
 import isEqual from 'lodash/isEqual'
 
+/** 
+ * Should use this function when your deps array value is object (handle deep comparision)
+ * Use normal useMemo hook when your deps array is primitive value (no need deep comparision)
+ */ 
 function useDeepMemo<T>(memoFn: () => T, key):T {
   const ref = useRef(null)
   if (!ref.current || !isEqual(key, ref.current.key)) {
