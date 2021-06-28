3c41c98a73f8b7957d14211a49e897a47a7cc3fa
tunght91 <hoangtung.utc@gmail.com>
2021-06-28 17:27:42 +0700

    update tier

--- src/views/Idos/components/ProjectDetail/TierDetails.tsx
+++ src/views/Idos/components/ProjectDetail/TierDetails.tsx
@@ -16,6 +16,7 @@ import {
   TelegramIcon,
   Progress,
   Image,
+  Button,
 } from 'common-uikitstrungdao'
 import { selectOpenPools, selectPool } from 'state/ido'
 import { formatPoolDetail, formatPoolTotalTierByChainID } from 'utils/formatPoolData'
@@ -28,7 +29,6 @@ const ImageContainer = styled.span`
   border-radius: 50%;
   width: 60px;
   height: 60px;
-  cursor: pointer;
   background-color: #e9e9e9;
   overflow: hidden;
   margin-right: 10px;
@@ -42,56 +42,117 @@ const TierCard = styled(Card)`
   }
 `
 
+const TIER_INFO = {
+  '1': {
+    name: 'Earth',
+    description: 'For every user, who holds less than 100 LUA or 100 TOMO',
+    icon: 'https://image.flaticon.com/icons/png/512/921/921490.png',
+    CTA: 'Buy 100 LUA to play with me on ground',
+  },
+  '2': {
+    name: 'Moon',
+    description: 'For every user, who holds from 100 LUA and 10.000 LUA',
+    icon: 'https://image.flaticon.com/icons/png/512/740/740860.png',
+    CTA: 'Buy more 1000 LUA to fly to the Moon',
+  },
+  '3': {
+    name: 'Galaxy',
+    description: 'For every user, who holds more than than 10.000 LUA',
+    icon: 'https://image.flaticon.com/icons/png/512/3919/3919942.png',
+    CTA: 'Buy 10.000 LUA to BREAK BORDER TOGETHER',
+  },
+}
+
 const TierDetails: React.FC<{
   index: string
 }> = ({ index }) => {
-  const [tiers, setIndex] = useState([0, 1, 2])
-
   const userTier = useSelector(selectUserTier)
+
   const pool = useSelector(selectPool(index))
 
-  const tiersss: IdoDetailInfo = useMemo(() => {
-    const chainIds = Object.keys(pool.index)
-    let result = pool.index[chainIds[0]]
-    for (let i = 1; i < chainIds.length; i++) {
-      result = formatPoolTotalTierByChainID(result, pool.index[chainIds[i]])
-    }
+  const tiersss: IdoDetailInfo[] = useMemo(() => {
+    if (pool) {
+      const chainIds = Object.keys(pool.index)
+      let result: IdoDetailInfo[] = pool.index[chainIds[0]]
+      for (let i = 1; i < chainIds.length; i++) {
+        result = formatPoolTotalTierByChainID(result, pool.index[chainIds[i]])
+      }
 
-    return result
+      return result
+    }
+    return []
   }, [pool])
 
-  const Tier = (data: IdoDetailInfo) => (
+  interface TierProps {
+    data: IdoDetailInfo
+  }
+  const Tier: React.FC<TierProps> = ({
+    data: { tier, totalAmountIDO, totalAmountPay, totalCommittedAmount, idoToken, payToken },
+  }) => (
     <TierCard>
       <CardBody style={{ height: '400px' }}>
         <Flex mb="15px" alignItems="center">
           <ImageContainer>
-            <Image src="https://luaswap.org/static/media/logo.d77b343b.png" alt="img" width={60} height={60} />
+            <Image src={TIER_INFO[tier].icon} alt="img" width={60} height={60} />
           </ImageContainer>
-          <Text fontSize="24px" bold>
-            Tier 1
+          <div>
+            <Text fontSize="24px" bold>
+              {TIER_INFO[tier].name}
+            </Text>
+            <Text fontSize="15px" bold>
+              Tier {tier}
+            </Text>
+          </div>
+        </Flex>
+        <Text mb="20px">{TIER_INFO[tier].description}</Text>
+        <Flex justifyContent="space-between">
+          <Text>Total {idoToken.symbol}</Text>
+          <Text bold>
+            {totalAmountIDO} {idoToken.symbol}
           </Text>
         </Flex>
-        <Text>For every user, who holds less than 100 LUA and 100 TOMO</Text>
-        <Flex justifyContent="flex-start" flexDirection="column">
-          <Text color="primary">Swap rate</Text>
-          <Text>1 BUSD = 10 BBANK</Text>
+        <Flex justifyContent="space-between">
+          <Text>Funds to raise</Text>
+          <Text bold>
+            {totalAmountPay} {payToken.symbol}
+          </Text>
         </Flex>
-        <Flex justifyContent="flex-start" flexDirection="column">
-          <Text color="primary">Cap</Text>
-          <Text>1000000</Text>
+        <Flex justifyContent="space-between">
+          <Text>Price per {idoToken.symbol}</Text>
+          <Text bold>
+            {Math.round((10000 * totalAmountIDO) / totalAmountPay) / 10000} {idoToken.symbol}/{payToken.symbol}
+          </Text>
         </Flex>
-        <Flex justifyContent="flex-end" flexDirection="column">
-          <Text color="primary">Access</Text>
-          <Text>Private</Text>
+        <Flex justifyContent="space-between">
+          <Text>Total committed</Text>
+          <Text bold>
+            {totalCommittedAmount} {payToken.symbol}
+          </Text>
         </Flex>
+        {userTier + 1 === parseInt(tier) ? (
+          <Button width="100%" mt="30px" disabled={userTier + 1 === parseInt(tier)}>
+            <Text bold>Your Tier. GET READY!</Text>
+            <Image
+              src="https://image.flaticon.com/icons/png/512/1067/1067357.png"
+              alt="img"
+              width={40}
+              height={40}
+              ml="20px"
+            />
+          </Button>
+        ) : (
+          <Button width="100%" mt="30px" variant="subtle">
+            {TIER_INFO[tier].CTA}
+          </Button>
+        )}
       </CardBody>
     </TierCard>
   )
 
   return (
     <Flex flexWrap="wrap" justifyContent="space-between">
-      {tiers.map((e, i) => (
-        <Tier data={e} key={e} />
+      {tiersss.map((e: IdoDetailInfo, i: number) => (
+        <Tier data={e} key={e.tier} />
       ))}
     </Flex>
   )
