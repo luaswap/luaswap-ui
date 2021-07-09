92c978f520a47c1d800ef42c3953a5d01e949d9f
tunght91 <hoangtung.utc@gmail.com>
2021-07-09 17:38:21 +0700

    update

--- src/views/Idos/DevTools.tsx
+++ src/views/Idos/DevTools.tsx
@@ -5,14 +5,19 @@ import LuaswapAbi from 'config/abi/LuaSwapIDO.json'
 import CreateForm from './components/DevTools/CreateForm'
 import ReadForm from './components/DevTools/ReadForm'
 import ApproveToken from './components/DevTools/ApproveToken'
+import IncreaseCap from './components/DevTools/IncreaseCap'
 
 const DevTools = () => {
   return (
     <Page>
       <Box>
         <ReadForm />
+        <br />
         <ApproveToken />
+        <br />
         <CreateForm abi={LuaswapAbi} />
+        <br />
+        <IncreaseCap abi={LuaswapAbi} />
       </Box>
     </Page>
   )
