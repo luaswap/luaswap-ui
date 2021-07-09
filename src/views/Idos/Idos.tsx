3e9cdcd11f4fdcf99f845a44bc22c9586c0cc6ce
Theo <trungdao912@gmail.com>
2021-07-09 17:27:15 +0700

    chore: adding guard for devtools

--- src/views/Idos/Idos.tsx
+++ src/views/Idos/Idos.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'contexts/Localization'
 import { Route, useRouteMatch, Switch } from 'react-router-dom'
+import AuthRoute from 'hooks/AuthRoute'
 import IdosInfo from './IdosInfo'
 import DevTools from './DevTools'
 import ProjectDetail from './components/ProjectDetail'
@@ -15,7 +16,7 @@ const Idos = () => {
         <ProjectDetail />
       </Route>
       <Route path={`${path}/dev-tools`}>
-        <DevTools />
+        <AuthRoute component={DevTools} />
       </Route>
       <Route path={`${path}`}>
         <IdosInfo />
