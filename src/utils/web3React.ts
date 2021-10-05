eae77414a5b8cdab3fe7f66f9a239f2a6e6412ce
Theo <trungdao912@gmail.com>
2021-10-05 15:19:13 +0700

    chore: remove BSC wallet

--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -1,14 +1,12 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
-import { BscConnector } from '@binance-chain/bsc-connector'
 import { ConnectorNames } from 'luastarter-uikits'
 import Web3 from 'web3'
 
 const POLLING_INTERVAL = 12000
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 
-export const injected = new InjectedConnector({ supportedChainIds: [1, 88, 89, 56] })
-const bscConnector = new BscConnector({ supportedChainIds: [56] })
+export const injected = new InjectedConnector({ supportedChainIds: [1, 88, 89] })
 
 const walletconnect = new WalletConnectConnector({
   rpc: { 1: NETWORK_URL },
@@ -20,7 +18,6 @@ const walletconnect = new WalletConnectConnector({
 export const connectorsByName = {
   [ConnectorNames.Injected]: injected,
   [ConnectorNames.WalletConnect]: walletconnect,
-  [ConnectorNames.BSC]: bscConnector,
 }
 
 export const getLibrary = (provider): Web3 => {
