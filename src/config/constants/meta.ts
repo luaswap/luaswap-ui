f16abbc100f7c998dab3f2ecf4d59a0cf92e37f8
Theo <trungdao912@gmail.com>
2021-10-11 13:42:33 +0700

    chore: fix web title

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -2,7 +2,7 @@ import { ContextApi } from 'contexts/Localization/types'
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'Luaswap',
+  title: 'LuaSwap',
   description: 'Luawswap interface',
 }
 
@@ -16,15 +16,15 @@ export const getCustomMeta = (path: string[], t: ContextApi['t']): PageMeta => {
   switch (routeName) {
     case '/':
       return {
-        title: `${t('Home')} | ${t('Luaswap')}`,
+        title: `${t('Home')} | ${t('LuaSwap')}`,
       }
     case 'farms':
       return {
-        title: `${t('Farms')} | ${t('Luaswap')}`,
+        title: `${t('Farms')} | ${t('LuaSwap')}`,
       }
     case 'idos':
       return {
-        title: `${t('Initial Dex Offering')} | ${t('Luaswap')}`,
+        title: `${t('Initial Dex Offering')} | ${t('LuaSwap')}`,
       }
     default:
       return null
