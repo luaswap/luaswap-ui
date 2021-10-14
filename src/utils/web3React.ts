9ae4a0015daf56e8547573747e68b23a8c769df9
Theo <trungdao912@gmail.com>
2021-10-14 11:42:14 +0700

    feat: adding BSC network

--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -1,5 +1,6 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { BscConnector } from '@binance-chain/bsc-connector'
 import { ConnectorNames } from 'luastarter-uikits'
 import getRpcUrl from 'utils/getRpcUrl'
 import Web3 from 'web3'
@@ -7,10 +8,11 @@ import Web3 from 'web3'
 const POLLING_INTERVAL = 12000
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 
-export const injected = new InjectedConnector({ supportedChainIds: [1, 88, 89] })
+export const injected = new InjectedConnector({ supportedChainIds: [1, 88, 89, 56] })
+const bscConnector = new BscConnector({ supportedChainIds: [56] })
 
 const walletconnect = new WalletConnectConnector({
-  rpc: { 1: getRpcUrl(1), 88: getRpcUrl(88), 89: getRpcUrl(89) },
+  rpc: { 1: getRpcUrl(1), 88: getRpcUrl(88), 89: getRpcUrl(89), 56: getRpcUrl(56) },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
   pollingInterval: POLLING_INTERVAL,
@@ -19,6 +21,7 @@ const walletconnect = new WalletConnectConnector({
 export const connectorsByName = {
   [ConnectorNames.Injected]: injected,
   [ConnectorNames.WalletConnect]: walletconnect,
+  [ConnectorNames.BSC]: bscConnector,
 }
 
 export const getLibrary = (provider): Web3 => {
