6878b5935a7459fc72e30347ed3d279505382309
Quy Nguyen <quynguyen@tomochain.com>
2022-01-19 09:48:45 +0700

    show message if don't login or wrong network, or no data stake page

--- src/views/Idos/components/Stake.tsx
+++ src/views/Idos/components/Stake.tsx
@@ -1,6 +1,7 @@
-import { Card } from 'luastarter-uikits'
+import { Card, Text } from 'luastarter-uikits'
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { useWeb3React } from '@web3-react/core'
 import IfoLayout from './IdoLayout'
 import StakeTable from './StakeTable'
 import StakeBox from './StakeTable/StakeBox'
@@ -17,12 +18,37 @@ const StakeContainer = styled(IfoLayout)`
   }
 `
 
+const NoticeWrap = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 300px;
+`
+
 const Stake: React.FC = () => {
+  const { account, chainId } = useWeb3React()
   return (
-    <StakeContainer>
-      <StakeTable />
-      <StakeBox />
-    </StakeContainer>
+    <>
+      {account ? (
+        <>
+          {chainId === 1 || chainId === 88 ? (
+            <StakeContainer>
+              <StakeTable />
+              <StakeBox />
+            </StakeContainer>
+          ) : (
+            <NoticeWrap>
+              <Text fontSize="16px">You need to select Tomochain Mainnet or Ethereum.</Text>
+            </NoticeWrap>
+          )}
+        </>
+      ) : (
+        <NoticeWrap>
+          <Text fontSize="16px">You need to connect your wallet.</Text>
+        </NoticeWrap>
+      )}
+    </>
   )
 }
 
--- src/views/Idos/components/StakeTable/index.tsx
+++ src/views/Idos/components/StakeTable/index.tsx
@@ -32,43 +32,51 @@ const StakeTable: React.FC = () => {
           <Spinner />
         </WrapperLoadingTable>
       ) : (
-        <Table>
-          <THead>
-            <TR>
-              <TD justifyContent="flex-start" width="23%">
-                <TextHeader fontSize="12px">TOKEN</TextHeader>
-              </TD>
-              <TD justifyContent="flex-end" width="21%">
-                <TextHeader fontSize="12px">QUANTITY</TextHeader>
-              </TD>
-              <TD justifyContent="flex-end" width="21%">
-                <TextHeader fontSize="12px">ESTIMATE LUA</TextHeader>
-              </TD>
-              <TD justifyContent="flex-end" width="28%">
-                <TextHeader fontSize="12px">LOCKED UNTIL</TextHeader>
-              </TD>
-              <TD justifyContent="flex-end" width="7%" />
-            </TR>
-          </THead>
-          <TBody>
-            {tokensLock.map((row, index) => (
-              <RowItem item={row} key={index.toString()} />
-            ))}
-          </TBody>
-          <TFooter>
-            <TR>
-              <TD justifyContent="flex-start" width="23%">
-                <Text fontSize="12px">Total</Text>
-              </TD>
-              <TD justifyContent="flex-end" width="21%" />
-              <TD justifyContent="flex-end" width="21%">
-                <Text fontSize="12px">{estTotalLua}</Text>
-              </TD>
-              <TD justifyContent="flex-end" width="28%" />
-              <TD justifyContent="flex-end" width="7%" />
-            </TR>
-          </TFooter>
-        </Table>
+        <>
+          {tokensLock.length > 0 ? (
+            <Table>
+              <THead>
+                <TR>
+                  <TD justifyContent="flex-start" width="23%">
+                    <TextHeader fontSize="12px">TOKEN</TextHeader>
+                  </TD>
+                  <TD justifyContent="flex-end" width="21%">
+                    <TextHeader fontSize="12px">QUANTITY</TextHeader>
+                  </TD>
+                  <TD justifyContent="flex-end" width="21%">
+                    <TextHeader fontSize="12px">ESTIMATE LUA</TextHeader>
+                  </TD>
+                  <TD justifyContent="flex-end" width="28%">
+                    <TextHeader fontSize="12px">LOCKED UNTIL</TextHeader>
+                  </TD>
+                  <TD justifyContent="flex-end" width="7%" />
+                </TR>
+              </THead>
+              <TBody>
+                {tokensLock.map((row, index) => (
+                  <RowItem item={row} key={index.toString()} />
+                ))}
+              </TBody>
+              <TFooter>
+                <TR>
+                  <TD justifyContent="flex-start" width="23%">
+                    <Text fontSize="12px">Total</Text>
+                  </TD>
+                  <TD justifyContent="flex-end" width="21%" />
+                  <TD justifyContent="flex-end" width="21%">
+                    <Text fontSize="12px">{estTotalLua}</Text>
+                  </TD>
+                  <TD justifyContent="flex-end" width="28%" />
+                  <TD justifyContent="flex-end" width="7%" />
+                </TR>
+              </TFooter>
+            </Table>
+          ) : (
+            <WrapperLoadingTable>
+              <Text fontSize="16px">You do not have any locked tokens.</Text>
+            </WrapperLoadingTable>
+          )}
+        </>
       )}
     </>
   )
