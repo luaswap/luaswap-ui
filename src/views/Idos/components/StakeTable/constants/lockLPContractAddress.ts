e8534c0f4b24d7123596e9bbb5b4e62fed107c44
Quy Nguyen <bk.nguyenvanquy@gmail.com>
2022-01-27 15:16:14 +0700

    update address lockLPContract on tomochain

--- src/views/Idos/components/StakeTable/constants/lockLPContractAddress.ts
+++ src/views/Idos/components/StakeTable/constants/lockLPContractAddress.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export const lockLPAddressOBJ = {
-  '88': '0x0a875D0cE6c8A6C49FCC848974C60d73f83CEcB6',
+  '88': '0x47aA27b2f154005fbEE7B05d050F6F992FcdFbB1',
   '1': '0x78846bd500AA06B65AE23C3168139A0CD0A5F73e',
 }
